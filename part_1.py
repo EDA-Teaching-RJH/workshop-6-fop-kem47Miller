c4f9934b0d26d6583f933f95ffc2f547eb3b5f90
kem47 <kem47@kent.ac.uk>
2026-02-09 14:05:03 +0000

    sahd

--- part_1.py
+++ part_1.py
@@ -1,1 +1,6 @@
-
+sample_bay=["Basalt","Silica","Iron","Dust"]
+print(sample_bay[0])
+print(sample_bay[len(sample_bay)-1])
+#first element in sample_bay list is numbered at 0/zero. so the four elements are numbered 0 to 3 respectively. to print the last element we say count the number of elements 1,2,3,4 then subtract 1 this gives 3 now find the element numbered at this number (find the 3rd element in the list). sample_bay goes before so we now which list we are using.
+print(len(sample_bay))
+# here we just ask to print number of elements in sample_bay list
